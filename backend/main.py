c971d038cab0cd39db52102db298d609d6a372a0
Kirill <kirill.zheludev@yandex.ru>
2022-10-25 03:28:37 +0300

    tasks were completed

--- backend/main.py
+++ backend/main.py
@@ -13,3 +13,4 @@ routers.init_app(app)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='127.0.0.1', port=settings.PORT, reload=True)
+
--- backend/routers/github_graph.py
+++ backend/routers/github_graph.py
@@ -1,32 +1,104 @@
+import urllib
+from urllib.request import urlopen
+import json
 import requests
 from fastapi import APIRouter
-
 from backend.config import settings
+from PIL import Image
 
 github_router = APIRouter(prefix='/github', tags=['Github'])
 
 
-@github_router.get("/{username}")
-async def root(username: str = "x4nth055"):
+@github_router.get("/{username}/the_last_follower")
+async def root(username: str = "krzhld"):
     token = settings.GITHUB_API_TOKEN
     headers = {
       "authorization": f"token {token}"
     } if token else {}
 
-    url = f"https://api.github.com/users/{username}/followers"
-
-    # Делаем запрос и получаем ответ
+    url = f"https://api.github.com/users/{username}"
     response = requests.get(url, headers=headers)
+    user_data = response.json()
+    followers_url = user_data["followers_url"]
+    json_url = urlopen(followers_url)
+    followers_data = json.loads(json_url.read())
+    last_follower = followers_data[-1]
 
     if response.status_code == 200:  # Проверка кода ответа
-        return response.json()
+        return last_follower
+    elif response.status_code == 401:
+        return {"status": 401,
+                "message": "Invalid authorization token"}  # Отправляем своё сообщение и код ошибки
     elif response.status_code == 404:
-        return {"status": 401, "message": "Ай-ай-ай, неверный токен авторизации!"}  # Отправляем своё сообщение и код ошибки
-    else:
-        return response.json()
+        return {"status": 404, "message": "Not found"}
+
+
+@github_router.get("/{username}/size")
+async def root(username: str = "krzhld"):
+    token = settings.GITHUB_API_TOKEN
+    headers = {
+      "authorization": f"token {token}"
+    } if token else {}
+
+    url = f"https://api.github.com/users/{username}"
+    response = requests.get(url, headers=headers)
+    user_data = response.json()
+    avatar_url = user_data["avatar_url"]
+
+    image = Image.open(urllib.request.urlopen(avatar_url))
+    width, height = image.size
+
+    if response.status_code == 200:  # Проверка кода ответа
+        return {"width": width, "height": height}
+    elif response.status_code == 401:
+        return {"status": 401,
+                "message": "Invalid authorization token"}  # Отправляем своё сообщение и код ошибки
+    elif response.status_code == 404:
+        return {"status": 404, "message": "Not found"}
+
+
+@github_router.get("/{username}/followers")
+async def root(username: str = "krzhld"):
+    token = settings.GITHUB_API_TOKEN
+    headers = {
+      "authorization": f"token {token}"
+    } if token else {}
+
+    url = f"https://api.github.com/users/{username}"
+    response = requests.get(url, headers=headers)
+    user_data = response.json()
+    followers_url = user_data['followers_url']
+    json_url = urlopen(followers_url)
+    followers_data = json.loads(json_url.read())
+
+    if response.status_code == 200:  # Проверка кода ответа
+        return followers_data
+    elif response.status_code == 401:
+        return {"status": 401, "message": "Invalid authorization token"}  # Отправляем своё сообщение и код ошибки
+    elif response.status_code == 404:
+        return {"status": 404, "message": "Not found"}
 
 
 @github_router.get("/hello/{name}")
 async def say_hello(name: str):
+    return {"message": f"Hello, {name}!"}
 
-    return {"message": f"Hello {name}"}
+
+@github_router.get("/{username}/name")
+async def root(username: str = "krzhld"):
+    token = settings.GITHUB_API_TOKEN
+    headers = {
+      "authorization": f"token {token}"
+    } if token else {}
+
+    url = f"https://api.github.com/users/{username}"
+    response = requests.get(url, headers=headers)
+    user_data = response.json()
+    name = user_data['name']
+
+    if response.status_code == 200:  # Проверка кода ответа
+        return {"name": name}
+    elif response.status_code == 401:
+        return {"status": 401, "message": "Invalid authorization token"}  # Отправляем своё сообщение и код ошибки
+    elif response.status_code == 404:
+        return {"status": 404, "message": "Not found"}
